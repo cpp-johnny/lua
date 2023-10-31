bc523beb1a630c4c0f618d3ae4a9fbfbf9b4b16f
boi <119715263+cpp-johnny@users.noreply.github.com>
2023-10-31 16:56:21 +0800

    Add files via upload

---
+++ first.lua
@@ -0,0 +1,23 @@
+-- Learning Lua!
+--
+-- this is a comment
+print("Hello")
+
+-- variable
+local x = 10
+print(x)
+
+-- increment
+local n = 1
+n = n + 1
+print(n)
+
+
+-- while loops
+local i = 1
+
+while i <= 3 do
+   print("hi 🍆 ")
+   i = i + 1
+end
+
